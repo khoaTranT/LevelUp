979cdef8ee964aae372b4b4fd7205ca90a91fa3b
khoaTranT <92763281+khoaTranT@users.noreply.github.com>
2025-03-01 17:28:52 -0500

    Update admin.py
    added admin insertions to the database for Meals

--- interface/levelup_django/levelup_django/meals/admin.py
+++ interface/levelup_django/levelup_django/meals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meal
+
+# Just allow admins to manually input new meals/ingredients.
+# Might not be useful for Meal.
+admin.site.register(Meal)
