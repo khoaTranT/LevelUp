8d264fe6833cf74f01b72552b4c62f0082efab57
khoaTranT <92763281+khoaTranT@users.noreply.github.com>
2025-03-01 17:33:47 -0500

    Update views.py
    created MealList with a basic function to fetch from the database

--- interface/levelup_django/levelup_django/meals/views.py
+++ interface/levelup_django/levelup_django/meals/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Meal
+from .serializers import MealSerializer
 # Create your views here.
+# TODO: Implement fetching data from the CDU database
+class MealList(APIView):
+    def get(self, request, format=None):
+        meals = Meal.objects.all() [0:5]
+        serializer = MealSerializer(meals, many=True)
+        return Response(serializer.data)
