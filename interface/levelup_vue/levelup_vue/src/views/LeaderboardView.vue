3c5e58b4510bbdbd5b5d749a6173ae63cb75d9e0
khoaTranT <92763281+khoaTranT@users.noreply.github.com>
2025-03-20 17:40:28 -0400

    Update LeaderboardView.vue
    Added tables for user rankings

--- interface/levelup_vue/levelup_vue/src/views/LeaderboardView.vue
+++ interface/levelup_vue/levelup_vue/src/views/LeaderboardView.vue
@@ -1,18 +1,62 @@
 <template>
-    <div class="column is-multiline">
-        
+    <div class="leaderboard-dashboard">
+      <div class="leaderboard">
+        <h2 class="card-title">Global Leaderboard</h2>
+
+        <div class="leaderboard-wrapper">
+          <table>
+            <thead>
+              <tr class="table">
+                <th class="table-item">Rank</th>
+                <th class="table-item">Username</th>
+                <th class="table-item">Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(user, index) in users" :key="user.score">
+                <td>{{ index+1 }}</td>
+                <td>{{ user.username }}</td>
+                <td>{{ user.score }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="leaderboard">
+        <h2 class="card-title" href="#">Private Leaderboard</h2>
+
+        <div class="leaderboard-wrapper">
+          <table>
+            <thead>
+              <tr class="table">
+                <th>Rank</th>
+                <th>Username</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(user, index) in fUsers" :key="user.score">
+                <td>{{ index+1 }}</td>
+                <td>{{ user.username }}</td>
+                <td>{{ user.score }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 
+//Replace if your implementation is different
 export default {
   name: 'LeaderboardView',
   data() {
     return {
-      leaderboard: [],
-      privateLeaderboard: []
+      users: [],
+      fUsers: [],
     }
   },
   components: {
@@ -22,26 +66,71 @@ export default {
     }
   },
   methods: {
-    getLeaderboard() { //STUB: Need to update to match component app in django. Fetches all user and their score/rankings.
-      axios
-        .get('api/v1/leaderboard') //Name of the django page storing these data (most likely to fetch all users)
-        .then(response=>{
-          this.profile = response.data
-        })
-        .catch(error=>{
-          console.log(error)
-        })
+    async getLeaderboard() { //TODO: Fetch all user and their score/rankings.
+      try {
+        axios.get('api/v1/leaderboard').then(response=>{
+            this.users = response.data
+          })
+      } catch (error) {
+        console.error('Error loading global user rankings:', error)
+      }
     },
-    getPLeaderboard() { //STUB: Need to update to match component app in django. Fetches all user and their score/rankings that are 'friend' with current user
-      axios
-        .get('api/v1/leaderboard') //Name of the django page storing these data (most likely to fetch all users)
-        .then(response=>{
-          this.profile = response.data
-        })
-        .catch(error=>{
-          console.log(error)
-        })
+    async getPLeaderboard() { //TODO: Fetches all user and their score/rankings that are 'friend' with current user
     }, 
   }
 }
 </script>
+
+<style scoped>
+.leaderboard-dashboard {
+  font-family: 'Segoe UI', Roboto, -apple-system, BlinkMacSystemFont, sans-serif;
+  max-width: 1500px;
+  margin: 2rem auto;
+  display: flex; 
+  justify-content: space-between; 
+  gap: 10px; 
+}
+
+.leaderboard {
+  flex: 1;
+  background: white;
+  border-radius: 16px;
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.08);
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+.card-title {
+  color: #333;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-top: 0;
+  margin-bottom: 20px;
+}
+
+.table {
+  margin: 4px 0 0;
+  padding: 0;
+  position: static;
+  width: 35%;
+  background: white;
+  border-radius: 20px;
+  border-collapse: collapse;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  max-height: 200px;
+  overflow-y: auto;
+  z-index: 10;
+}
+
+th, td {
+  color:#333;
+  font-size: 1rem;
+  font-weight: 600;
+  margin-top: 0;
+  margin-bottom: 20px;
+}
+
+th:not(:last-child), td:not(:last-child) {
+  border-right: 1px solid rgb(0, 0, 0);
+}
+</style>
